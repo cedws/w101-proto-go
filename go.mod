a86e198b0f7cae891bf2b2ce7c69701f9b8e723e
Connor Edwards <38229097+cedws@users.noreply.github.com>
2024-11-05 21:58:57 +0000

    feat(updater): Add Root.wad service XML extraction

--- go.mod
+++ go.mod
@@ -2,14 +2,4 @@ module github.com/cedws/w101-proto-go
 
 go 1.23.2
 
-require github.com/cedws/w101-client-go v0.0.0-20241031190349-3a7061904813
-
-require (
-	github.com/beevik/etree v1.4.1 // indirect
-	github.com/inconshreveable/mousetrap v1.1.0 // indirect
-	github.com/sirupsen/logrus v1.9.3 // indirect
-	github.com/spf13/cobra v1.8.1 // indirect
-	github.com/spf13/pflag v1.0.5 // indirect
-	golang.org/x/sys v0.26.0 // indirect
-	golang.org/x/text v0.19.0 // indirect
-)
+require github.com/cedws/w101-client-go v0.0.0-20241102001526-287541fb546d
--- internal/updater/main.go
+++ internal/updater/main.go
@@ -2,14 +2,28 @@ package main
 
 import (
 	"context"
+	"fmt"
+	"io"
+	"log"
+	"log/slog"
 	"net/http"
 	"net/url"
+	"os"
+	"slices"
+	"time"
 
 	"github.com/cedws/w101-client-go/proto"
+	"github.com/cedws/w101-client-go/wad"
 	"github.com/cedws/w101-proto-go/patch"
 )
 
 const patchServer = "patch.us.wizard101.com:12500"
+
+var messageFiles = []string{
+	"GameMessages.xml",
+	"PatchMessages.xml",
+	"LoginMessages.xml",
+}
 
 type patchHandler struct {
 	patch.PatchService
@@ -21,10 +35,80 @@ func (p patchHandler) LatestFileListV2(m patch.LatestFileListV2) {
 }
 
 func main() {
-	Patch(context.Background())
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancel()
+
+	if err := downloadMessageFiles(ctx); err != nil {
+		log.Fatal(err)
+	}
+
+	slog.Info("Downloaded message files")
 }
 
-func Patch(ctx context.Context) error {
+func downloadMessageFiles(ctx context.Context) error {
+	if err := downloadRootWAD(ctx); err != nil {
+		return err
+	}
+
+	wadArchive, err := wad.Open("Root.wad")
+	if err != nil {
+		return err
+	}
+	defer wadArchive.Close()
+
+	return extractMessages(wadArchive)
+}
+
+func extractMessages(wadArchive *wad.Archive) error {
+	for entry := range wadArchive.Entries() {
+		if !slices.Contains(messageFiles, entry.Path) {
+			continue
+		}
+
+		slog.Info("Extracting message file", "path", entry.Path)
+
+		entryReader, err := wadArchive.Entry(entry)
+		if err != nil {
+			return err
+		}
+
+		msgFile, err := os.Create(entry.Path)
+		if err != nil {
+			return err
+		}
+		defer msgFile.Close()
+
+		if _, err := io.Copy(msgFile, entryReader); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func downloadRootWAD(ctx context.Context) error {
+	wadReader, err := latestRootWAD(ctx)
+	if err != nil {
+		return err
+	}
+	defer wadReader.Close()
+
+	wadFile, err := os.Create("Root.wad")
+	if err != nil {
+		return err
+	}
+	defer wadFile.Close()
+
+	slog.Info("Downloading Root.wad")
+
+	if _, err := io.Copy(wadFile, wadReader); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func latestRootWAD(ctx context.Context) (io.ReadCloser, error) {
 	fileListCh := make(chan patch.LatestFileListV2)
 
 	r := proto.NewMessageRouter()
@@ -32,38 +116,45 @@ func Patch(ctx context.Context) error {
 
 	client, err := proto.Dial(ctx, patchServer, r)
 	if err != nil {
-		return err
+		return nil, err
 	}
 	defer client.Close()
 
+	slog.Info("Connected to patch server", "server", patchServer)
+
 	c := patch.NewPatchClient(client)
 	if err := c.LatestFileListV2(&patch.LatestFileListV2{}); err != nil {
-		return err
+		return nil, err
 	}
 
 	select {
 	case fileList := <-fileListCh:
-		return downloadFileList(ctx, fileList)
+		slog.Info("Received latest file list", "url", fileList.ListFileURL)
+		return requestRootWAD(ctx, fileList)
 	case <-ctx.Done():
-		return ctx.Err()
+		return nil, ctx.Err()
 	}
 }
 
-func downloadFileList(ctx context.Context, fileList patch.LatestFileListV2) error {
+func requestRootWAD(ctx context.Context, fileList patch.LatestFileListV2) (io.ReadCloser, error) {
 	wadURL, err := url.JoinPath(fileList.URLPrefix, "Data/GameData/Root.wad")
 	if err != nil {
-		return err
+		return nil, err
 	}
 
 	req, err := http.NewRequestWithContext(ctx, http.MethodGet, wadURL, nil)
 	if err != nil {
-		return err
+		return nil, err
 	}
 
 	resp, err := http.DefaultClient.Do(req)
 	if err != nil {
-		return err
+		return nil, err
 	}
 
-	return resp.Body.Close()
+	if resp.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("unexpected status code: %d", resp.StatusCode)
+	}
+
+	return resp.Body, err
 }
