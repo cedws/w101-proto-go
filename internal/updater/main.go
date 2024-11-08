b6c4282c110accce2591cd4173eab2291d5b7965
Connor Edwards <38229097+cedws@users.noreply.github.com>
2024-11-08 20:08:57 +0000

    feat(updater): Add codegen

--- internal/updater/main.go
+++ internal/updater/main.go
@@ -9,9 +9,12 @@ import (
 	"net/http"
 	"net/url"
 	"os"
+	"path/filepath"
 	"slices"
+	"strings"
 	"time"
 
+	"github.com/cedws/w101-client-go/codegen"
 	"github.com/cedws/w101-client-go/proto"
 	"github.com/cedws/w101-client-go/wad"
 	"github.com/cedws/w101-proto-go/pkg/patch"
@@ -19,10 +22,33 @@ import (
 
 const patchServer = "patch.us.wizard101.com:12500"
 
-var messageFiles = []string{
+var protocolFiles = []string{
+	"AISClientMessages.xml",
+	"BaseMessages.xml",
+	"ExtendedBaseMessages.xml",
 	"GameMessages.xml",
-	"PatchMessages.xml",
 	"LoginMessages.xml",
+	"PatchMessages.xml",
+	"PetMessages.xml",
+	"ScriptDebuggerMessages.xml",
+	"TestManagerMessages.xml",
+	"WizardMessages.xml",
+	"Messages/CantripsMessages.xml",
+	"Messages/CatchAKeyMessages.xml",
+	"Messages/ChooChooZooMessages.xml",
+	"Messages/ConcentrationMessages.xml",
+	"Messages/DoodleDougMessages.xml",
+	"Messages/Dueling_DiegoMessages.xml",
+	"Messages/HotShotsMessages.xml",
+	"Messages/HousingMessages.xml",
+	"Messages/MoveBehaviorMessages.xml",
+	"Messages/PhysicsBehaviorMessages.xml",
+	"Messages/PotionMotionMessages.xml",
+	"Messages/QuestMessages.xml",
+	"Messages/ShockALockMessages.xml",
+	"Messages/SkullRidersMessages.xml",
+	"Messages/SoblocksMessages.xml",
+	"Messages/WizCombatMessages.xml",
 }
 
 type patchHandler struct {
@@ -38,14 +64,14 @@ func main() {
 	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
 	defer cancel()
 
-	if err := downloadMessageFiles(ctx); err != nil {
+	if err := generateProtocolFiles(ctx); err != nil {
 		log.Fatal(err)
 	}
 
 	slog.Info("Downloaded message files")
 }
 
-func downloadMessageFiles(ctx context.Context) error {
+func generateProtocolFiles(ctx context.Context) error {
 	if err := downloadRootWAD(ctx); err != nil {
 		return err
 	}
@@ -56,34 +82,65 @@ func downloadMessageFiles(ctx context.Context) error {
 	}
 	defer wadArchive.Close()
 
-	return extractMessages(wadArchive)
-}
+	protocols, err := extractProtocols(wadArchive)
+	if err != nil {
+		return err
+	}
 
-func extractMessages(wadArchive *wad.Archive) error {
-	for entry := range wadArchive.Entries() {
-		if !slices.Contains(messageFiles, entry.Path) {
-			continue
+	for _, protocol := range protocols {
+		var (
+			name     = strings.ToLower(protocol.Service)
+			dirname  = filepath.Join("pkg", name)
+			filename = filepath.Join(dirname, name+".go")
+		)
+
+		if err := os.MkdirAll(dirname, 0o755); err != nil {
+			return err
 		}
 
-		slog.Info("Extracting message file", "path", entry.Path)
-
-		entryReader, err := wadArchive.Entry(entry)
+		goFile, err := os.Create(filename)
 		if err != nil {
 			return err
 		}
 
-		msgFile, err := os.Create(entry.Path)
-		if err != nil {
-			return err
-		}
-		defer msgFile.Close()
-
-		if _, err := io.Copy(msgFile, entryReader); err != nil {
+		slog.Info("Generating protocol file", "path", filename)
+		if err := codegen.Generate(goFile, name, protocol); err != nil {
 			return err
 		}
 	}
 
 	return nil
+}
+
+func extractProtocols(wadArchive *wad.Archive) ([]codegen.Protocol, error) {
+	protocols := make([]codegen.Protocol, 0)
+
+	for entry := range wadArchive.Entries() {
+		if !slices.Contains(protocolFiles, entry.Path) {
+			continue
+		}
+
+		slog.Info("Extracting protocols file", "path", entry.Path)
+
+		entryReader, err := wadArchive.Entry(entry)
+		if err != nil {
+			return nil, err
+		}
+
+		protoBytes, err := io.ReadAll(entryReader)
+		if err != nil {
+			return nil, err
+		}
+
+		proto, err := codegen.UnmarshalProtocol(protoBytes)
+		if err != nil {
+			return nil, err
+		}
+
+		protocols = append(protocols, proto)
+	}
+
+	return protocols, nil
 }
 
 func downloadRootWAD(ctx context.Context) error {
